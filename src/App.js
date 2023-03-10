da533eacfd07a8f645e5e7492f76eb92365104f2
VladaBolotova <vladislavabolotova@gmail.com>
2023-03-09 21:26:10 -0500

    starting signup page

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 import { useReducer } from 'react';
 import selectionReducer from './context/selectionReducer';
 import { GlobalContext } from './context/context';
-import Earth from './components/Earth';
+import SignUp from './components/SignUp';
+// import Earth from './components/Earth';
 
 function App() {
-  const [selection, selectionDispatch] = useReducer(selectionReducer,null)
+  // const [selection, selectionDispatch] = useReducer(selectionReducer,null)
   return (
-    <GlobalContext.Provider value={{selection, selectionDispatch}}>
-      <div style={{position:'absolute',color:'white',zIndex:1,padding:'1rem'}}>
-        {selection ? selection.properties.NAME : 'No country selected'}
-      </div>
-      <Earth />
-    </GlobalContext.Provider>
+    // <GlobalContext.Provider value={{selection, selectionDispatch}}>
+    //   <div style={{position:'absolute',color:'white',zIndex:1,padding:'1rem'}}>
+    //     {selection ? selection.properties.NAME : 'No country selected'}
+    //   </div>
+    //   <Earth />
+    // </GlobalContext.Provider>
+   <div>
+
+ <SignUp />
+  
+  </div> 
   );
 }
 
