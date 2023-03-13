e80b3189148a10a923f5e96ae180e7bd009e8382
VladaBolotova <vladislavabolotova@gmail.com>
2023-03-13 18:41:03 -0400

    changing routes

--- src/App.js
+++ src/App.js
@@ -6,30 +6,30 @@ import {
   Routes,
   Route,
 } from "react-router-dom";
-import SignupPage from './pages/Signup';
-import LoginPage from './pages/Login';
-import Header from './components/Header'
-// import Earth from './components/Earth';
+// import SignupPage from './pages/Signup';
+// import LoginPage from './pages/Login';
+// import Header from './components/Header'
+import Earth from './components/Earth';
 
 function App() {
-  // const [selection, selectionDispatch] = useReducer(selectionReducer,null)
+  const [selection, selectionDispatch] = useReducer(selectionReducer,null)
   return (
-    // <GlobalContext.Provider value={{selection, selectionDispatch}}>
-    //   <div style={{position:'absolute',color:'white',zIndex:1,padding:'1rem'}}>
-    //     {selection ? selection.properties.NAME : 'No country selected'}
-    //   </div>
-    //   <Earth />
-    // </GlobalContext.Provider>
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LoginPage/>} />
-            <Route path="/signup" element={<SignupPage/>} />
-        </Routes>
-      </BrowserRouter>
-    </div>
-  </div>
+    <GlobalContext.Provider value={{selection, selectionDispatch}}>
+      <div style={{position:'absolute',color:'white',zIndex:1,padding:'1rem'}}>
+        {selection ? selection.properties.NAME : 'No country selected'}
+      </div>
+      <Earth />
+    </GlobalContext.Provider>
+  //   <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+  //   <div className="max-w-md w-full space-y-8">
+  //    <BrowserRouter>
+  //       <Routes>
+  //           <Route path="/login" element={<LoginPage/>} />
+  //           <Route path="/signup" element={<SignupPage/>} />
+  //       </Routes>
+  //     </BrowserRouter>
+  //   </div>
+  // </div>
 
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import {Link} from 'react-router-dom';
+import image from './globe.png'
 
 export default function Header({
     heading,
@@ -11,8 +12,8 @@ export default function Header({
             <div className="flex justify-center">
                 <img 
                     alt=""
-                    className="h-14 w-14"
-                    src="https://ik.imagekit.io/pibjyepn7p9/Lilac_Navy_Simple_Line_Business_Logo_CGktk8RHK.png?ik-sdk-version=javascript-1.4.3&updatedAt=1649962071315"/>
+                    className="h-20 w-20"
+                    src={image}/>
             </div>
             <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                 {heading}
