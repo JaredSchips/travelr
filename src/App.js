5951b799a4e94392dd50290872b8775fb4753224
Omar Correa <omarcorrea378@yahoo.com>
2023-03-13 19:01:33 -0400

    started chat

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import { useReducer } from 'react';
-import selectionReducer from './context/selectionReducer';
-import { GlobalContext } from './context/context';
-import Earth from './components/Earth';
+import { useReducer } from "react";
+import selectionReducer from "./context/selectionReducer";
+import { GlobalContext } from "./context/context";
+import Earth from "./components/Earth";
+import Chat from "./components/Chat";
 
 function App() {
-  const [selection, selectionDispatch] = useReducer(selectionReducer,null)
+  const [selection, selectionDispatch] = useReducer(selectionReducer, null);
   return (
-    <GlobalContext.Provider value={{selection, selectionDispatch}}>
-      <div style={{position:'absolute',color:'white',zIndex:1,padding:'1rem'}}>
-        {selection ? selection.properties.NAME : 'No country selected'}
+    <GlobalContext.Provider value={{ selection, selectionDispatch }}>
+      <div
+        style={{
+          position: "absolute",
+          color: "white",
+          zIndex: 1,
+          padding: "1rem",
+        }}
+      >
+        {selection ? selection.properties.NAME : "No country selected"}
       </div>
       <Earth />
+      <div className="center">
+      <Chat />
+      </div>
     </GlobalContext.Provider>
   );
 }
