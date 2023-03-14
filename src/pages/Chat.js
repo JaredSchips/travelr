39082dc7e8f2406ff44c3f8ab2f1e72e462a4e9a
Omar Correa <omarcorrea378@yahoo.com>
2023-03-13 20:19:21 -0400

    useless

---
+++ src/pages/Chat.js
@@ -0,0 +1,14 @@
+import Chat from "../components/Chat";
+
+export default function ChatBox(){
+    return(
+        <>
+            <Chat
+              heading="Signup to create an account"
+              paragraph="Already have an account? "
+              linkName="Chat"
+              linkUrl="/chat"
+            />
+        </>
+    )
+}
