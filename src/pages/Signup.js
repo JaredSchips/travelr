073d63074dbeec19808931a801b01a399e638c1b
Omar Correa <omarcorrea378@yahoo.com>
2023-03-13 21:38:56 -0400

    updates

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,5 +1,5 @@
-import Header from "../components/header";
-import Signup from "../components/signup";
+import Header from "../components/Header";
+import Signup from "../components/Signup";
 
 export default function SignupPage() {
   return (
