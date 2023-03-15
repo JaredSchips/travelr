a13b554b6864449a6c1bd8928be8bf150326f56a
JaredSchips <jared.schips@gmail.com>
2023-03-14 23:13:42 -0400

    add buttons to navbar and remove console logs

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,6 +2,8 @@ import Earth from "../components/Earth"
 import { useContext, useEffect } from "react"
 import { GlobalContext } from "../context/context"
 import { useState } from "react"
+import { BiMessageSquareDetail } from "react-icons/bi";
+import { AiOutlinePlusCircle, AiOutlineUnorderedList, AiOutlineHeart } from "react-icons/ai";
 
 
 export default function Homepage() {
@@ -13,11 +15,11 @@ export default function Homepage() {
     if (!selection) {console.log('null');return}
     fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${selection.properties.name}`)
       .then(res => res.json())
-      .then(description => {console.log(description);setSummary(description.extract)})
+      .then(description => setSummary(description.extract))
     
     fetch(`https://en.wikipedia.org/api/rest_v1/page/media-list/${selection.properties.name}`)
       .then(res => res.json())
-      .then(result => {console.log(result);setImageUrl(result.items[0].srcset[0].src)})
+      .then(result => setImageUrl(result.items[0].srcset[0].src))
   }, [selection])
 
   return (
@@ -25,7 +27,7 @@ export default function Homepage() {
       <div>
       </div>
       {selection && <div className="absolute left-0 top-0 bg-black z-10 w-3/12 h-full opacity-80 text-white">
-        <img className="min-w-full" alt="ny" src={imageUrl} />
+        <img className="min-w-full" alt="A city" src={imageUrl} />
         <div className="grid gap-2 p-4">
           <div>
             <h2 className="text-2xl">
@@ -38,6 +40,32 @@ export default function Homepage() {
           <p className="text-sm pr-2 overflow-auto max-h-48">
             {summary}
           </p>
+          <div className="w-full pt-5 flex justify-evenly">
+            <a
+              href={`#`}
+              className="text-3xl w-fit"
+            >
+            <AiOutlineHeart />
+            </a>
+            <a
+              href={`#`}
+              className="text-3xl w-fit"
+            >
+            <BiMessageSquareDetail />
+            </a>
+            <a
+              href={`#`}
+              className="text-3xl w-fit"
+            >
+            <AiOutlinePlusCircle />
+            </a>
+            <a
+              href={`#`}
+              className="text-3xl w-fit"
+            >
+            <AiOutlineUnorderedList />
+            </a>
+          </div>
         </div>
         
       </div>}
