472c1263c63b85cb3e57b19a86540ab9fd042554
VladaBolotova <vladislavabolotova@gmail.com>
2023-03-15 16:01:42 -0400

    profile page start

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import { GlobalContext } from "./context/context";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/Signup";
 import LoginPage from "./pages/Login";
-import Header from "./components/header";
+import Header from "./components/Header";
 import Earth from "./components/Earth";
 import NavigationBar from "./components/NavigationBar";
+import Chat from "./components/Chat";
+import ProfilePage from "./pages/Profile";
 
 function App() {
   const [selection, selectionDispatch] = useReducer(selectionReducer, null);
@@ -28,6 +30,8 @@ function App() {
         </div>
 
         <Earth />
+        <ProfilePage />
+        <Chat />
       </GlobalContext.Provider>
       <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
         <div className="max-w-md w-full space-y-8">
---
+++ src/pages/Profile.js
@@ -0,0 +1,28 @@
+
+import profileImage from "./profile.jpg";
+
+const ProfilePage = () => {
+  return (
+    <div className="flex flex-col items-center">
+      <img src={profileImage} alt="Profile" className="w-40 h-40 rounded-full" />
+      <h1 className="mt-4 text-2xl font-bold">John Doe</h1>
+      <p className="mt-2 text-gray-500">Software Engineer</p>
+      <div className="mt-4 w-64 flex justify-between">
+        <div>
+          <p className="font-bold">Followers</p>
+          <p>10K</p>
+        </div>
+        <div>
+          <p className="font-bold">Following</p>
+          <p>100</p>
+        </div>
+        <div>
+          <p className="font-bold">Posts</p>
+          <p>50</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProfilePage;
