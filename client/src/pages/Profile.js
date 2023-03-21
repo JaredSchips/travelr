70729926cefdca895dfceb9e464fbdb7db831aa4
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-20 20:29:28 -0400

    Edited profile.js

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,28 +1,29 @@
 import React from "react";
-import {useState} from 'react'
+import { useState } from "react";
 import profileImage from "./profile-image.jpg";
-import { FaHeart, FaChartLine , FaEnvelopeOpenText, FaGlobeEurope } from "react-icons/fa";
+import {
+  FaHeart,
+  FaChartLine,
+  FaEnvelopeOpenText,
+  FaGlobeEurope,
+} from "react-icons/fa";
 import BucketList from "../components/BucketList";
 import FavoriteCountries from "../components/FavList";
-import Chat from '../components/Chat';
-
+import Chat from "../components/Chat";
 
 const ProfilePage = () => {
-  const [currentPage,setCurrentPage]=useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
-  const renderPage=()=>{
-    switch(currentPage) {
+  const renderPage = () => {
+    switch (currentPage) {
       case 2:
-      return <BucketList />;
-      case 3: 
-      return <FavoriteCountries />;
+        return <BucketList />;
+      case 3:
+        return <FavoriteCountries />;
       case 4:
-      return <Chat />;
-    
-
+        return <Chat />;
     }
-  }
-
+  };
 
   const buttonStyling = `flex space-x-3 mr-2 font-semibold bg-gradient-to-r from-blue-600 via-indigo-700 to-indigo-900 
   text-gray-100 rounded-full ring-5 ring-purple-200 px-8 py-2 
@@ -31,43 +32,44 @@ const ProfilePage = () => {
 
   return (
     <>
-  
-    <div className="flex flex-col items-center">
-      <img src={profileImage} alt="Profile" className="w-40 h-40 rounded-full" />
-      <h1 className="mt-4 text-2xl font-bold">John Doe</h1>
-      <p className="mt-2 text-gray-500">Traveler</p>
-      <div className="mt-4  flex justify-between">
-      <div className=''>
-<a href="/favorite" onClick={()=>setCurrentPage(3)}><button
-          type='submit'
-          className={buttonStyling}> 
-          <FaHeart size='2.5rem' />
-        </button></a>
+      <div className="flex flex-col items-center">
+        <img
+          src={profileImage}
+          alt="Profile"
+          className="w-40 h-40 rounded-full"
+        />
+        <h1 className="mt-4 text-2xl font-bold">John Doe</h1>
+        <p className="mt-2 text-gray-500">Traveler</p>
+        <div className="mt-4  flex justify-between">
+          <div className="">
+            <a href="/favorite" onClick={() => setCurrentPage(3)}>
+              <button type="submit" className={buttonStyling}>
+                <FaHeart size="2.5rem" />
+              </button>
+            </a>
+          </div>
+          <a href="/stats" onClick={() => setCurrentPage(3)}>
+            <button type="submit" className={buttonStyling}>
+              <FaChartLine size="2.5rem" />
+            </button>
+          </a>
+          <div className=""></div>
+          <a href="/bucket" onClick={() => setCurrentPage(2)}>
+            {" "}
+            <button type="submit" className={buttonStyling}>
+              <FaGlobeEurope size="2.5rem" />
+            </button>
+          </a>
+          <a href="/chat" onClick={() => setCurrentPage(4)}>
+            {" "}
+            <button type="submit" className={buttonStyling}>
+              <FaEnvelopeOpenText size="2.5rem" />
+            </button>
+          </a>
+          <div className=""></div>
+        </div>
       </div>
-      <div className=''>
-        <button
-          type='submit'
-          className={buttonStyling}>  
-          <FaChartLine size='2.5rem' />
-        </button>
-      </div>
-      <div className=''>    
-      </div>
-      <a href="/bucket" onClick={()=>setCurrentPage(2)}> <button
-          type='submit'
-          className={buttonStyling}>
-          <FaGlobeEurope size='2.5rem' />
-        </button></a> 
-        <a href="/chat" onClick={()=>setCurrentPage(4)}> <button
-          type='submit'
-          className={buttonStyling}>
-          <FaEnvelopeOpenText size='2.5rem' />
-        </button></a> 
-      <div className=''>
-      </div>
-      </div>
-    </div>
-    {renderPage}
+      {renderPage}
     </>
   );
 };
