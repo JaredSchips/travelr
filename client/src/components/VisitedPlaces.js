0bb56e6a55789235219cd8fae0f36dcc13c5903b
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-21 00:25:27 -0400

    Added Footer.js & footer.css

--- client/src/components/VisitedPlaces.js
+++ client/src/components/VisitedPlaces.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import image from "./globe.png";
 import "./VisitedPlaces.css";
 
@@ -11,6 +12,24 @@ function VisitedPlaces() {
     "Berlin, Germany",
   ]);
   const [newPlace, setNewPlace] = useState("");
+  const [placeImages, setPlaceImages] = useState([]);
+
+  const UNSPLASH_API_KEY = "80B5-eXWcv50qvBAsd-jGPth_omIc7BfqsgLLClSta8";
+
+  useEffect(() => {
+    async function fetchImages() {
+      const images = await Promise.all(
+        places.map(async (place) => {
+          const response = await axios.get(
+            `https://api.unsplash.com/search/photos?query=${place}&client_id=${UNSPLASH_API_KEY}&per_page=1`
+          );
+          return response.data.results[0]?.urls.small || null;
+        })
+      );
+      setPlaceImages(images);
+    }
+    fetchImages();
+  }, [places]);
 
   const handleAddPlace = () => {
     if (newPlace.trim() !== "") {
@@ -30,15 +49,26 @@ function VisitedPlaces() {
       </div>
       <div className="bg-white-500 mt-2 flex justify-center items-center bg-globe bg-no-repeat bg-cover">
         <div className="max-w-md w-full rounded-lg bg-white shadow-lg p-3">
-          <h2 className="text-2xl font-bold mb-4">Travelr | Places Visited</h2>
+          <h2 className="text-2xl font-bold mb-4">
+            Travelr | Places Visited 📍
+          </h2>
           <div className="grid grid-cols-2 gap-4 bg-purple-500 p-4 rounded-lg mb-4">
             {places.map((place, index) => (
               <div
                 key={index}
-                className="place-block bg-white text-purple-500 p-2 rounded-lg"
+                className="place-block bg-white text-white p-2 rounded-lg relative"
               >
-                {"📍 "}
-                {place}
+                {placeImages[index] && (
+                  <img
+                    src={placeImages[index]}
+                    alt={place}
+                    className="w-full h-full object-cover absolute inset-0 z-0 rounded-lg"
+                  />
+                )}
+                <div className="z-10 relative place-label">
+                  {"📍 "}
+                  {place}
+                </div>
               </div>
             ))}
           </div>
