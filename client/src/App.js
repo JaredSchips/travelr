19658d42082a9259047943f5e5ccc32514e1b657
MartinCespedes <martin.cespedes002@mymdc.net>
2023-03-22 18:06:32 -0400

    Added live link to README.md

--- client/src/App.js
+++ client/src/App.js
@@ -8,8 +8,8 @@ import {
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
 import Homepage from "./pages/Homepage";
 import SignupPage from "./pages/Signup";
@@ -25,15 +25,15 @@ import Footer from "./components/Footer";
 import { useState } from "react";
 
 const httpLink = createHttpLink({
-  uri: '/graphql',
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem('id_token');
+  const token = localStorage.getItem("id_token");
   return {
     headers: {
       ...headers,
-      authorization: token ? `Bearer ${token}` : '',
+      authorization: token ? `Bearer ${token}` : "",
     },
   };
 });
