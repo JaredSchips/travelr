65946d604a3148164dc7dd0657003e7bc7112402
JaredSchips <jared.schips@gmail.com>
2023-03-21 22:59:53 -0400

    Make profile page into single page hub

--- client/src/App.js
+++ client/src/App.js
@@ -50,7 +50,7 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <GlobalContext.Provider value={{ selection, selectionDispatch }}>
-        <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
           <div className="max-w-md w-full space-y-8">
             <BrowserRouter>
               <NavigationBar iconBlack={iconBlack} />
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -10,28 +10,31 @@ import {
 import BucketList from "../components/BucketList";
 import FavoriteCountries from "../components/FavList";
 import Chat from "../components/Chat";
+import Statistics from "../components/Statistics";
 
 import { useQuery } from '@apollo/client';
 import { ME } from "../utils/queries";
 
 const ProfilePage = ({ setIconsBlack }) => {
   setIconsBlack(true);
-  const [currentPage, setCurrentPage] = useState(1);
+  const [currentPage, setCurrentPage] = useState(0);
   const { data } = useQuery(ME)
-  const me = data.me
+  const me = data?.me
 
   const renderPage = () => {
     switch (currentPage) {
-      case 2:
-        return <BucketList />;
-      case 3:
+      case 1:
         return <FavoriteCountries />;
-      case 4:
-        return <Chat />;
+      case 2:
+        return <Statistics />;
+      case 3:
+        return <BucketList />;
       default:
         return
     }
   };
+
+
 
   const buttonStyling = `flex space-x-3 mr-2 font-semibold bg-gradient-to-r from-blue-600 via-indigo-700 to-indigo-900 
   text-gray-100 rounded-full ring-5 ring-purple-200 px-8 py-2 
@@ -46,38 +49,32 @@ const ProfilePage = ({ setIconsBlack }) => {
           alt="Profile"
           className="w-40 h-40 rounded-full"
         />
-        <h1 className="mt-4 text-2xl font-bold">{me.firstName} {me.lastName}</h1>
-        <p className="mt-2 text-gray-500">{me.username}</p>
+        <h1 className="mt-4 text-2xl font-bold">{me?.firstName} {me?.lastName}</h1>
+        <p className="mt-2 text-gray-500">{me?.username}</p>
         <div className="mt-4  flex justify-between">
           <div className="">
-            <a href="/favorite" onClick={() => setCurrentPage(3)}>
+            <a href="#" onClick={() => setCurrentPage(1)}>
               <button type="submit" className={buttonStyling}>
                 <FaHeart size="2.5rem" />
               </button>
             </a>
           </div>
-          <a href="/stats" onClick={() => setCurrentPage(3)}>
+          <a href="#" onClick={() => setCurrentPage(2)}>
             <button type="submit" className={buttonStyling}>
               <FaChartLine size="2.5rem" />
             </button>
           </a>
           <div className=""></div>
-          <a href="/bucket" onClick={() => setCurrentPage(2)}>
+          <a href="#" onClick={() => setCurrentPage(3)}>
             {" "}
             <button type="submit" className={buttonStyling}>
               <FaGlobeEurope size="2.5rem" />
             </button>
           </a>
-          <a href="/chat" onClick={() => setCurrentPage(4)}>
-            {" "}
-            <button type="submit" className={buttonStyling}>
-              <FaEnvelopeOpenText size="2.5rem" />
-            </button>
-          </a>
           <div className=""></div>
         </div>
       </div>
-      {renderPage}
+      {renderPage()}
     </>
   );
 };
